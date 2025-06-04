c7b64f53fe734f4d0dcf87269510d1b5fbcf6fde
unknown <562183876@qq.com>
2025-06-04 22:53:20 +0800

    fix image

--- next.config.js
+++ next.config.js
@@ -5,6 +5,17 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   output: 'standalone',
+  images: {
+    // 明确允许 public/assets/images 下的所有图片被优化
+    localPatterns: [
+      {
+        // 注意：这里的 pathname 要和你请求的 url 完全匹配
+        pathname: '/assets/images/**',
+      },
+    ],
+    // 如果你只是临时不想优化，也可以加下面一行跳过优化（会让 <Image> 直接输出 <img>）
+    // unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
